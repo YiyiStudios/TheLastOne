42e12a69e73af98f18899b0c13a91931bf899de4
Bryest <u201511892@upc.edu.pe>
2018-10-30 08:21:41 -0500

    Respaldo

--- TheLastOne/Assets/Scripts/Stamina.cs
+++ TheLastOne/Assets/Scripts/Stamina.cs
@@ -7,18 +7,7 @@ public class Stamina : MonoBehaviour {
 
     public int staminabar;
     public Image imagelifebar;
-    float time;
 
-<<<<<<< HEAD
-    // Use this for initialization
-    void Start() {
-        player = GetComponent<PlayerController>();
-        time = 1f;
-    }
-
-    // Update is called once per frame
-    void Update() {
-=======
 	// Use this for initialization
 	void Start () {
 		
@@ -26,24 +15,11 @@ public class Stamina : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
->>>>>>> 73616fbcc84c0beee9eda29e1646857c1b21d323
         StaminaBar();
     }
 
     public void StaminaBar()
     {
-<<<<<<< HEAD
-        if (player.mov != Vector2.zero)
-        {
-            imagelifebar.fillAmount = Mathf.InverseLerp(staminabar, 0, time++);
-        }
-        if (imagelifebar.fillAmount != 0 && Input.GetKeyDown(KeyCode.K))
-        {
-            imagelifebar.fillAmount = staminabar;
-            GameController.instance.PlayerLose();
-        }
-=======
         imagelifebar.fillAmount = Mathf.InverseLerp(staminabar, 0, Time.fixedTime);
->>>>>>> 73616fbcc84c0beee9eda29e1646857c1b21d323
     }
 }
