391032e8b971bd7a50d5ee00cbb2deb546a5f73e
Bryest <u201511892@upc.edu.pe>
2018-11-07 11:02:44 -0500

    Enemy1
    Idle blend tree complete

--- TheLastOne/Assets/Scripts/Enemy1Controller.cs
+++ TheLastOne/Assets/Scripts/Enemy1Controller.cs
@@ -6,44 +6,52 @@ public class Enemy1Controller : MonoBehaviour
 {
     enum STATE { IDLE, CHASE }
     STATE state = STATE.IDLE;
-    Vector3 a;
+    Vector3 torotate;
     public float rotate;
     TimeController time;
     VisualField vf;
     public Transform player;
-    [Range(1f,20f)]
+    [Range(1f, 20f)]
     public float velocity;
+    Animator anim;
+    int contanim = 0;
+
 
     // Use this for initialization
     void Start()
     {
+        anim = GetComponent<Animator>();
         player = player.GetComponent<Transform>();
         vf = GetComponent<VisualField>();
         time = gameObject.AddComponent<TimeController>();
-        time.totaltime = 2f;
+        time.totaltime = 1.3f;
         time.TurnOn();
-        a.z = transform.rotation.eulerAngles.z;
+        torotate.z = transform.rotation.eulerAngles.z;
     }
     // Update is called once per frame
     void Update()
     {
         States();
+        //       Animation_States();
     }
     void Rotate()
     {
         if (time.Finished())
         {
             time.Run();
-            a.z += rotate;
-            transform.rotation = Quaternion.Euler(a);
+            //  torotate.z += rotate;
+            //  transform.rotation = Quaternion.Euler(torotate);
+            AnimationRotate();
             time.TurnOn();
         }
+
+
     }
     void Chasing()
     {
         if (vf.listvisibletarget.Count != 0)
         {
-            transform.position = Vector2.MoveTowards(transform.position, vf.listvisibletarget[0].transform.position, Time.deltaTime *velocity);
+            transform.position = Vector2.MoveTowards(transform.position, vf.listvisibletarget[0].transform.position, Time.deltaTime * velocity);
         }
     }
     void States()
@@ -73,5 +81,40 @@ public class Enemy1Controller : MonoBehaviour
         }
     }
 
+    Vector2 DirectionVector()
+    {
+        return (player.position - transform.position).normalized;
+    }
 
+
+    void Animation_States(Vector2 mov)
+    {
+        anim.SetFloat("Horizontal", mov.x);
+        anim.SetFloat("Vertical", mov.y);
+        anim.SetFloat("Magnitude", mov.magnitude);
+    }
+    void AnimationRotate()
+    {
+        switch (contanim)
+        {
+            case 0:
+                anim.SetFloat("Horizontal", 1);
+                anim.SetFloat("Vertical", 0);
+                break;
+            case 1:
+                anim.SetFloat("Horizontal", 0);
+                anim.SetFloat("Vertical", 1);
+                break;
+            case 2:
+                anim.SetFloat("Horizontal", -1);
+                anim.SetFloat("Vertical", 0);
+                break;
+            case 3:
+                anim.SetFloat("Horizontal", 0);
+                anim.SetFloat("Vertical", -1);
+                break;
+
+        }
+        contanim++;
+    }
 }
