01aedc6cbe9f0cc24bddce458ed366d998c06b82
PandaGO13 <43046156+PandaGO13@users.noreply.github.com>
2018-11-07 12:05:45 -0500

    Inventario - Parte 2
    Los items seleccionados en la vista items se muestran el inventario dentro del juego.

---
+++ TheLastOne/Assets/InventoryUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class InventoryUI : MonoBehaviour {
+
+    public Transform itemsParent;
+
+    Inventory_ inventory;
+
+    InventorySlot[] slots;
+
+	// Use this for initialization
+	void Start () {
+
+        inventory = Inventory_.instance;
+
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+
+        for (int i=0; i < inventory.items.Count; i++)
+        {
+            slots[i].AddItem(inventory.items[i]);            
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
