ff6a3155709290518f67e060ba5c18479240dbd8
Bryest <u201511892@upc.edu.pe>
2018-11-06 21:53:13 -0500

    Testing Runner
    Play Mode, carga el escenario de testing

---
+++ TheLastOne/Assets/Scripts/TestsPlayMode/NewTestScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class NewTestScript {
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator StaminaTestSimplePasses()
+    {
+        // Use the Assert class to test conditions.
+        SetUpScene();
+        yield return new WaitForSeconds(5f);
+    }
+    void SetUpScene()
+    {
+        MonoBehaviour.Instantiate(Resources.Load<GameObject>("Player"));
+        MonoBehaviour.Instantiate(Resources.Load<GameObject>("Canvas1"));
+        MonoBehaviour.Instantiate(Resources.Load<GameObject>("CMvcam1"));
+    }
+}
