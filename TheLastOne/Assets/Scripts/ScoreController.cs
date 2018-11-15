638722bd8288f136df71f7873d1c6359cb28775a
Bryest <u201511892@upc.edu.pe>
2018-11-15 15:18:00 -0500

    Score
    Coins with Score

---
+++ TheLastOne/Assets/Scripts/ScoreController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreController : MonoBehaviour {
+    int value = 5;
+    public GameObject scorePoints;
+    public int totalscore=0;
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            totalscore += value;
+            scorePoints.GetComponent<Text>().text = totalscore.ToString();
+            gameObject.SetActive(false);
+        }
+    }
+}
