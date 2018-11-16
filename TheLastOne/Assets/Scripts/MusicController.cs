6af88833856d784f013ebca5dd6842bf09e0791c
Bryest <u201511892@upc.edu.pe>
2018-11-16 08:00:18 -0500

    Audio
    Music Manager Complete
    coin sound complete

---
+++ TheLastOne/Assets/Scripts/MusicController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    
+    public static MusicController instance;
+    #region AudioClips
+    //public AudioClip walkClip;
+    public AudioClip coinClip;
+    //public AudioClip item1Clip;
+    //public AudioClip item2Clip;
+    #endregion  
+    public AudioSource backgroundAudioSource;
+    public AudioSource soundAudioSource;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(instance);
+        }
+    }
+    public void CoinEffect()
+    {
+        PlaySound(coinClip);
+    }
+    //public void WalkClip()
+    //{
+    //    PlaySound(walkClip);
+    //}
+    public void PlaySound(AudioClip ac)
+    {
+        soundAudioSource.clip = ac;
+        soundAudioSource.Play();
+    }
+}
--- TheLastOne/Assets/Scripts/Score.cs
+++ TheLastOne/Assets/Scripts/Score.cs
@@ -4,21 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Score : MonoBehaviour {
     int value = 5;
-	// Use this for initialization
-	void Start () {
-
-    }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             ScoreController.totalscore += value;
-          //  scorePoints.GetComponent<Text>().text = totalscore.ToString();
+            MusicController.instance.CoinEffect();
             gameObject.SetActive(false);
         }
     }
