062f6da4a07f47ed00a369d462bc42c541fd6cb6
Bryest <u201511892@upc.edu.pe>
2018-11-20 15:39:57 -0500

    Enemy2
    rotation clockwise
    Movement with sprite sheet

--- TheLastOne/Assets/Scripts/Enemy1Controller.cs
+++ TheLastOne/Assets/Scripts/Enemy1Controller.cs
@@ -14,20 +14,33 @@ public class Enemy1Controller : MonoBehaviour
     [SerializeField] Transform player;
     [Range(1f, 20f)] [SerializeField] private float velocity;
     Animator anim;
-    private int contanim = 0;
+    private int contanimA = 0;
+    private int contanimB = -3;
     [HideInInspector]
     public Vector3 initialposition;
+
+    public int kindenemy;
+    
     // Use this for initialization
     void Start()
     {
         anim = GetComponent<Animator>();
         player = player.GetComponent<Transform>();
-        vf = objvf.GetComponent<VisualField>();
-        objdetector = objdetector.GetComponent<Transform>();
-        time = gameObject.AddComponent<TimeController>();
+         vf = objvf.GetComponent<VisualField>();
+         objdetector = objdetector.GetComponent<Transform>();
+            
+          time = gameObject.AddComponent<TimeController>();
         time.totaltime = 1f;
         time.TurnOn();
         initialposition = transform.position;
+    }
+    void KindRotation(int kindEnemy)
+    {
+
+        if (kindEnemy == 0) { AnimationRotateA(); }
+
+
+        if (kindEnemy == 1) { AnimationRotateB(); }
     }
     // Update is called once per frame
     void Update()
@@ -39,7 +52,10 @@ public class Enemy1Controller : MonoBehaviour
         if (time.Finished())
         {
             time.Run();
-            AnimationRotate();
+
+            KindRotation(kindenemy);
+
+
             time.TurnOn();
         }
     }
@@ -83,9 +99,9 @@ public class Enemy1Controller : MonoBehaviour
         anim.SetFloat("Vertical", mov.y);
         anim.SetFloat("Magnitude", mov.magnitude);
     }
-    void AnimationRotate()
+    void AnimationRotateA()
     {
-        switch (contanim)
+        switch (contanimA)
         {
             case 0:
                 anim.SetFloat("Horizontal", 1);
@@ -116,7 +132,44 @@ public class Enemy1Controller : MonoBehaviour
                 objdetector.position = transform.localPosition + new Vector3(0, -0.34f, 0);
                 break;
         }
-        contanim++;
-        if (contanim >= 4) { contanim = 0; }
+
+        contanimA++;
+        if (contanimA >= 4) { contanimA = 0; }
+    }
+    void AnimationRotateB()
+    {
+        switch (contanimB)
+        {
+            case 0:
+                anim.SetFloat("Horizontal", 1);
+                anim.SetFloat("Vertical", 0);
+                vf.transform.rotation = Quaternion.Euler(0, 0, 0);
+                objdetector.position = transform.localPosition + new Vector3(0.65f, 0.75f, 0);
+                objdetector.rotation = Quaternion.Euler(0, 0, 90);
+                break;
+            case -1:
+                anim.SetFloat("Horizontal", 0);
+                anim.SetFloat("Vertical", 1);
+                vf.transform.rotation = Quaternion.Euler(0, 0, 90);
+                objdetector.rotation = Quaternion.Euler(0, 0, 180);
+                objdetector.position = transform.localPosition + new Vector3(0, 1.8f, 0);
+                break;
+            case -2:
+                anim.SetFloat("Horizontal", -1);
+                anim.SetFloat("Vertical", 0);
+                vf.transform.rotation = Quaternion.Euler(0, 0, 180);
+                objdetector.rotation = Quaternion.Euler(0, 0, 270);
+                objdetector.position = transform.localPosition + new Vector3(-0.8f, 0.75f, 0);
+                break;
+            case -3:
+                anim.SetFloat("Horizontal", 0);
+                anim.SetFloat("Vertical", -1);
+                vf.transform.rotation = Quaternion.Euler(0, 0, 270);
+                objdetector.rotation = Quaternion.Euler(0, 0, 0);
+                objdetector.position = transform.localPosition + new Vector3(0, -0.34f, 0);
+                break;
+        }
+        contanimB++;                                    
+        if (contanimB >= 1) { contanimB = -3; }
     }
 }
